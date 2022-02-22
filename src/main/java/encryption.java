fdb435be9d05380311952123307fa44c562af388
Ronald Kolum <kolumronnie@gmail.com>
2022-02-22 06:41:54 +0300

    Added encryption functionality

--- src/main/java/encryption.java
+++ src/main/java/encryption.java
@@ -1,3 +1,21 @@
 public class encryption {
+    private String message;
+    private int key;
+    private String output;
+
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static String runEncryption(String plainText, int shiftKey)
+    {
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+        for (int i = 0; i < plainText.length(); i++)
+        {
+            int charPosition = ALPHABET.indexOf(plainText.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = ALPHABET.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
 
 }
