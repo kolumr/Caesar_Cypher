26dd85bd2207c566bf938ff8a95db38d15c793a1
Ronald Kolum <kolumronnie@gmail.com>
2022-02-22 06:41:14 +0300

    Added test for shift of 1

--- src/test/java/encryptionTest.java
+++ src/test/java/encryptionTest.java
@@ -3,5 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class encryptionTest {
+    @Test
+    public void testEncryptWithShift1(){
+        int key = 1;
+        String message = "H";
+        String expectedResultMessage = "i";
+        encryption encrypt = new encryption();
+        String actualResultMessage = encrypt.runEncryption(message, key);
+
+        assertEquals(expectedResultMessage, actualResultMessage);
+
+    }
 
 }
