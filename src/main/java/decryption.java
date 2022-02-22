f2fc0343fb9772fe14feaa59296f8479f7f246c1
Ronald Kolum <kolumronnie@gmail.com>
2022-02-22 06:53:59 +0300

    Added decryption functionality

--- src/main/java/decryption.java
+++ src/main/java/decryption.java
@@ -1,7 +1,20 @@
 public class decryption {
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    public static String runDecryption(String plainText, int shiftKey)
+    public static String runDecryption(String cipherText, int shiftKey)
     {
-        return null;
+        cipherText = cipherText.toLowerCase();
+        String plainText = "";
+        for (int i = 0; i < cipherText.length(); i++)
+        {
+            int charPosition = ALPHABET.indexOf(cipherText.charAt(i));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0)
+            {
+                keyVal = ALPHABET.length() + keyVal;
+            }
+            char replaceVal = ALPHABET.charAt(keyVal);
+            plainText += replaceVal;
+        }
+        return plainText;
     };
 }
