9e33695f6b267f1767bde5a29a2228a5d7f44556
Ronald Kolum <kolumronnie@gmail.com>
2022-02-22 06:51:03 +0300

    Added decryption test for a shift of 1

--- src/main/java/decryption.java
+++ src/main/java/decryption.java
@@ -1,3 +1,7 @@
 public class decryption {
-
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static String runDecryption(String plainText, int shiftKey)
+    {
+        return null;
+    };
 }
--- src/test/java/decryptionTest.java
+++ src/test/java/decryptionTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class decryptionTest {
+    @Test
+    public void testEncryptWithShift1(){
+        int key = 1;
+        String message = "H";
+        String expectedResultMessage = "g";
+        decryption decrypt = new decryption();
+        String actualResultMessage = decrypt.runDecryption(message, key);
+
+        assertEquals(expectedResultMessage, actualResultMessage);
+
+    }
 
 }
